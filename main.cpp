d7346e83ff880e286a98e7f53ff1dafbfd682bc0
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2023-09-21 14:29:34 +0800

    SNIA Mode done

--- main.cpp
+++ main.cpp
@@ -36,18 +36,25 @@ int main(int argc, char * argv[])
     exp = stoi(vec[2]), KPP = stoi(vec[3]), cmd_per_group = stoi(vec[4]);
     Mode md = (vec[1] == "-k") ?Mode::by_key :Mode::by_rand;
 
-    // calculation
+    // calculate system spec
     Maxlba = pow(2, exp) - 1;
     int L = ceil(exp / log2(KPP)) + 1;   // total level num
     if(Maxlba < 3) { 
         cout << "too small\n"; 
         return 0; 
     }
+
+    // print spec
+    for(auto i: vec)
+        ofs << i << " ";
+    ofs << "\n\n";
+
 // test code
-    
+    // ...
 // Different Modes
     // Key Mode
     if(vec[0] == "-k") {
+        cout << "Key Mode\n";
         // pure key sanitization
         ofs << "size k_mean k_min k_max | p_mean p_min p_max | #page_result\n";
         for(int i = 0; i < exp; i ++) {
@@ -88,47 +95,86 @@ int main(int argc, char * argv[])
             for(auto k: record_page_num) {
                 ofs << k.first << "*" << k.second << " ";
             }
-
             ofs << endl;
         }
-    }
+    }   // end of Key Mode
+
     // SNIA Mode ./main -s -r <exp> <KPP> <cmd>
     else if(vec[0] == "-s") {
-        cout << "snia\n";
+        cout << "SNIA Mode\n";
         // input file
         ifstream ifs;
         ifs.open("s17_01_all.txt");
-        ofs << "W_KPN\n";
 
-        for(int i = 0; i < 3; i ++) {
-            Tree zns(Maxlba, KPP, L);
-            int write_page_num = 0;
-            // SNIA write to full
-            while(zns.write_pointer <= Maxlba) {
-                // check ifs
-                if(ifs.eof()) {
-                    ifs.clear();
-                    ifs.seekg(0);   // use this to get back to beginning of file
-                    cout << "hit file end\n";
+        ofs << "size write_pn | k_mean k_min k_max | p_mean p_min p_max | #page_result\n";
+        for(int i = 0; i < 8; i ++) {
+            int size = pow(2, i);
+            map<int, int> record_key_num;   // (#updated keys, number of repitition)
+            map<int, int> record_page_num;   // (#R/W pages, number of repitition)
+            // map<int, int> record_write_pn;  // write page num (# write pages, number of repitition)
+            float sum_key_num = 0, sum_page_num = 0;    // for sanitize
+            float write_sum_page_num = 0;   // for write
+
+            for(int j = 0; j < cmd_per_group; j ++) {
+                Tree zns(Maxlba, KPP, L);
+                int counter = 0;
+                // write to full
+                while(zns.write_pointer <= Maxlba) {
+                    // check ifs
+                    if(ifs.eof()) {
+                        ifs.clear();
+                        ifs.seekg(0);   // use this to get back to beginning of file
+                        cout << "hit file end\n";
+                    }
+                    // get SNIA size
+                    string s;
+                    int size;
+                    ifs >> s;
+                    if(s == "") continue;
+                    size = stoi(s);
+                    // write
+                    counter += zns.write_data(size);
                 }
-                // get SNIA size
-                string s;
-                int size;
-                ifs >> s;
-                if(s == "") continue;
-                size = stoi(s);
-                // write
-                write_page_num += zns.write_data(size);
+                write_sum_page_num += counter;
+                cout << "write cost: " << counter << endl;
+
+                // sanitize
+                if(zns.tree[zns.MLI].size() != Maxlba + 1) {
+                    cout << "Error:trying to sanitize a not full zone\n";
+                    return 1;
+                }
+                pair<int, int> data = cmd_gen(md, size, KPP, Maxlba);
+                pair<int, int> keynum_pagenum = zns.sanitize(data);
+
+                record_key_num[keynum_pagenum.first] ++;
+                sum_key_num += keynum_pagenum.first;
+
+                record_page_num[keynum_pagenum.second] ++;
+                sum_page_num += keynum_pagenum.second;
             }
-            ofs << "wp: " << zns.write_pointer << " pn: " << write_page_num << endl;
+
+            // output: "size write_pn | k_mean k_min k_max | p_mean p_min p_max | #page_result";
+            // calculate updated key
+            int min = record_key_num.begin()->first, max = record_key_num.rbegin()->first;
+            float mean = sum_key_num / cmd_per_group;
+
+            ofs << "2^" << i << " ";    // size
+            ofs << write_sum_page_num / cmd_per_group;  //write_pn
+            ofs << " | " << mean << " " << min << " " << max << " | ";  // k
+            
+            // calculate R/W pages
+            min = record_page_num.begin()->first, max = record_page_num.rbegin()->first;
+            mean = sum_page_num / cmd_per_group;
+            ofs << mean << " " << min << " " << max << " | ";   // p
+
+            // #page_result
+            for(auto k: record_page_num) {
+                ofs << k.first << "*" << k.second << " ";
+            }
+            ofs << endl;
         }
-        
-        ofs << endl;
-    }
-    // print spec
-    for(auto i: vec)
-        ofs << i << " ";
-    ofs << endl;
+    }   // end of SNIA Mode
+
     // close output file
     ofs.close();
     return 0;
